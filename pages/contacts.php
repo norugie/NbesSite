bb46aa6db8319c9bf770d654a6b9d1314fe386a5
Rugie Ann Barrameda <norugie015@gmail.com>
2022-03-10 08:37:58 -0800

    minor changes

--- pages/contacts.php
+++ pages/contacts.php
@@ -33,6 +33,23 @@
                         </div>
                     <?php endforeach; ?>
                 </div>
+                <div class="heading">
+                    <h3>Teachers</h3>
+                </div>
+                <div class="row text-center">
+                    <?php $teachers = $site->teacherList($database, $schoolContent); ?>
+                    <?php foreach($teachers as $teacher): ?>
+                        <div class="col-md-4 justify-content">
+                            <div class="box-simple">
+                                <h3 class="h4"><?php echo $teacher['firstname'] . " " . $teacher['lastname']; ?><br><span style="font-size:15px;"><?php echo $teacher['position']?></span></h3>
+                                <p style="color: #000!important;">
+                                    <strong>Email Address: </strong><a href="mailto:<?php echo $teacher['email']; ?>"><?php echo $teacher['email']; ?></a><br>
+                                    <strong>Phone Number: </strong> <?php echo $teacher['phone']; ?>
+                                </p>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
             </div>
         </div>
     </section>
